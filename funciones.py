7703d53886694bb7b8e6e3f574a8d7e924b1c031
RCL0666 <re.colipe@duocuc.cl>
2025-06-19 15:48:00 -0400

    ultimo commit : agregar funciones de validacion

--- funciones.py
+++ funciones.py
@@ -3,7 +3,7 @@ ventas = []
 editorales = ("planeta ", "anagrama ", "penguin ramdom house")
 
 def registrar_libro():
-    codigo = int(input("codigo del libro: "))
+    codigo = validar_codigo()
     titulo = input("titulo: ")
     autor = input("autor: ")
     
@@ -13,8 +13,8 @@ def registrar_libro():
     cod_editorial = int(input("seleccione un editorial: "))
     editoral = editorales[cod_editorial-1]
     
-    precio = float(input("precio del libro: "))
-    stock = int (input("cantidad de stock:"))
+    precio = validar_precio()
+    stock = validar_entero_positivo()
     
     
     libro = {
@@ -41,11 +41,11 @@ def ver_catalago():
             print(f"stock: {libro['stock']}")
 
 def vender_libros():
-    codigo = int(input("codigo del libro que desa comprar: "))
+    codigo = validar_codigo_unico(input("codigo del libro a comprar: "))
     for libro in libros:
         if libro['codigo'] == codigo:
             print(f"titulo: {libro['titulo']}, stock: {libro['stock']}")
-            cantidad = int(input("cantidad a comprar: "))
+            cantidad = validar_entero_positivo("cantidad a comprar: ")
             if cantidad <= libro['stock']:
                 libro['stock'] -= cantidad
                 total = cantidad * libro['precio']
@@ -71,3 +71,36 @@ def ver_ventas():
             print(f"titulo: {venta['titulo']}")
             print(f"precio: {venta['precio']}")
             print(f"editorial: {venta['editorial']}")
+
+def validar_codigo():
+    while True:
+        cod=input("Ingrese codigo del libro:")
+        if cod.isdigit() and int (cod) >= 1:
+            return int(cod)
+        print (" ERROR: el codigo debe ser un numero entero mayor o igual a 1.")
+
+def validar_codigo_unico(codigo ):
+    for libro in libros:
+        if libro['codigo'] == codigo:
+            print("ERROR: ya existe un libro con ese codigo")
+            return False
+    return True
+
+def validar_precio():
+    while True:
+        try:
+            precio = float(input("Ingrese precio del libro:"))
+            if precio > 0:
+                return precio
+            else:
+                print("ERROR: el precio debe ser mayor a 0")
+        except ValueError:
+            print("ERROR: debe ingresar un numero decimal valido")
+            
+def validar_entero_positivo(mensaje="ingrese un numero" ):
+    while True:
+        num = input(mensaje)
+        if num.isdigit() and int(num) > 0:
+            return int(num)
+        print("ERROR: deve ingresar un numero entero mayor a 0.")
+        
